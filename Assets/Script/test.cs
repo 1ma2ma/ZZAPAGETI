cfbc2fb19d80be9ff2c504d2eef3c26d8851ce2d
Kimyoujin <zkadbwls246@gmail.com>
2024-05-26 18:27:42 +0900

    테스트 수정

--- Assets/Script/test.cs
+++ Assets/Script/test.cs
@@ -7,12 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("test");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
